e5d6393564decb5ffe650d1bc8b247a745cd2897
Agustin985 <143972230+Agustin985@users.noreply.github.com>
2024-03-11 21:28:24 -0300

    products (#2)

---
+++ ProductManager.js
@@ -0,0 +1,28 @@
+class ProductManager{
+    static #products = [];
+    create(data) {
+        const product = {
+            id: ProductManager.#products.length === 0 ? 1 : ProductManager.#products[ProductManager.#products.length - 1].id+1,
+            title : data.title,
+            photo: data.photo,
+            category : data.category,
+            price : data.price,
+            stock : data.stock
+        };
+        ProductManager.#products.push (product);
+        console.log("productocreado")
+    }
+   read(){
+    return ProductManager.#products
+   }
+}
+
+const gestorDeProductos = new ProductManager()
+gestorDeProductos.create({
+    title : "zapatilla",
+    photo: "zapatilla.jpg",
+    category : "calzado",
+    price : 90000,
+    stock : 100
+}) //crear 4 productos mas
+console.log(gestorDeProductos.read())
